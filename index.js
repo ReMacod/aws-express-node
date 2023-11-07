6814188eaa42e151c26ccb084118f3a91d1268a8
MarkoBitencCode <marko.bitenc@codelivery.team>
2023-11-07 20:00:43 +0100

    testing deiply

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) =>
   res.json({
-    message: "Hello Codelivery 👀, Api running....",
+    message: "Hello Codelivery 👀, Api running.... and deploy done",
   })
 );
 app.listen(process.env.PORT || 5001);
