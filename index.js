12a7dd89be94bead353e9e655a3acbc18c3424d2
MarkoBitencCode <marko.bitenc@codelivery.team>
2023-11-12 01:42:02 +0100

    testing to deploy EC2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) =>
   res.json({
-    message: "Hello Codelivery 👀, Api running.... and deploy done",
+    message: "Hello Codelivery 👀, Api running.... and deploy done testing",
   })
 );
 app.listen(process.env.PORT || 5001);
