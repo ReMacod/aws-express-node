b2ae0e611499df26fe3305e521c1a6e4004c4be9
MarkoBitencCode <marko.bitenc@codelivery.team>
2023-11-12 18:26:25 +0100

    testing stage and prod for runner

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) =>
   res.json({
-    message: "Hello Codelivery 👀, Api running for stage done",
+    message:
+      "Hello Codelivery 👀, Api running for stage and prod manual -> done",
   })
 );
 app.listen(process.env.PORT || 5001);
