0f8536cdf3b2744f417da8ce63a776134318ab67
MarkoBitencCode <marko.bitenc@codelivery.team>
2023-11-25 20:08:05 +0100

    add db connection

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-import express from "express";
+const path = require("path");
+const express = require("express");
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DB_PORT,
+});
+
 const app = express();
 app.get("/", (req, res) =>
   res.json({
     message: "Hello Codelivery 👀, Api running ... for stage and prod",
   })
 );
+
+app.get("/users", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.json(result);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    res.json(result.rows);
+  }
+});
+
 app.listen(process.env.PORT || 5001);
